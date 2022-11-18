a6e3b6e0a44f4c79ccbe0777420256a9d14985b0
Enes <m.enes.durak@gmail.com>
2022-11-18 12:30:02 +0300

    Database created

---
+++ app/src/main/java/com/menesdurak/notesapp/database/NoteDatabase.kt
@@ -0,0 +1,32 @@
+package com.menesdurak.notesapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.menesdurak.notesapp.models.Note
+import com.menesdurak.notesapp.utilities.DATABASE_NAME
+
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+abstract class NoteDatabase: RoomDatabase() {
+
+    abstract fun getNoteDao() : NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/menesdurak/notesapp/utilities/Constants.kt
@@ -0,0 +1,3 @@
+package com.menesdurak.notesapp.utilities
+
+const val DATABASE_NAME = "note_database"
